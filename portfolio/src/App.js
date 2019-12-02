9a98c96d5f10dbd66ed9dd20dde3cb786937148a
Jenn Soderborg <jsoderborg7@gmail.com>
2019-12-02 10:14:57 -0700

    additional styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,19 +8,23 @@ import Resume from './components/Resume';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
-import './SASS/app.scss';
+// import './SASS/app.scss';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div className="header">
+        <Header />
+      </div>
       <div className='routes'>
         <Route exact path='/' component={Home} />
         <Route path='/projects' component={Projects} />
         <Route path='/resume' component={Resume} />
         <Route path='/contact' component={Contact} />
       </div>
-      <Footer className="footer" />
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
