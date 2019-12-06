3d5e6c09ef0985c01371917e233586a656a1ed2e
Jenn Soderborg <jsoderborg7@gmail.com>
2019-12-06 10:54:33 -0700

    added resume png and download button to resume tab, still needs work

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import '../SASS/resume.scss';
+
+import ResumeImg from '../assets/resume.png'
 
 const Resume = () =>{
   return(
-    <p>this is where my resume will go</p>
+    <div className="resume-container">
+      <img src={ResumeImg} alt="Resume" />
+      <a href='https://drive.google.com/file/d/1KosFNaF5NmhxDCuZYeWfKVdIDWB3u-Wn/view?usp=sharing'
+      target='_blank'>
+        <button>Download</button>
+      </a>
+    </div>
   )
 }
 
