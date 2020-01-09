b23cadc91ea4ec4533e6e4b0241ac61f05d9f510
Jenn Soderborg <jsoderborg7@gmail.com>
2020-01-09 11:43:58 -0700

    changing styling, got advice from Peter

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -8,17 +8,22 @@ const Header = () =>{
   return(
     <div className="header-container">
       <div className="text-container">
-        <img src={Profile} alt="Profile picture"/>
+        {/* <img src={Profile} alt="Profile picture"/> */}
         <div className="text">
-          <h1>Jenn Soderborg</h1>
-          <h2>Web Developer</h2>
+          <div className="greeting">
+            <p>Hi! I'm<h1>Jenn Soderborg</h1></p>
+            
+          </div>
+          <div className="small-text">
+            <p>I'm a<h2>Web Developer</h2>from Utah.</p>
+            <p>When I'm not coding, I love all things water, baking, and music. Thanks for stopping by!</p>      </div>        
         </div>
       </div>
       <div className="nav-container">
-        <NavLink to='/' className='navlinks'>Home</NavLink>
         <NavLink to='/projects' className='navlinks'>Projects</NavLink>
         <NavLink to='/resume' className='navlinks'>Resume</NavLink>
         <NavLink to='/contact' className='navlinks'>Contact</NavLink>
+        <NavLink to='/' className='navlinks'>About</NavLink>
       </div>
     </div>
   );
--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import '../SASS/resume.scss';
+import {PDFReader} from 'reactjs-pdf-reader';
 
 import ResumeImg from '../assets/resume.png'
+import ResumePDF from '../assets/resume.pdf'
 
 const Resume = () =>{
   return(
     <div className="resume-container">
-      <img src={ResumeImg} alt="Resume" />
+      {/* <img src={ResumeImg} alt="Resume" /> */}
+      <PDFReader
+      className="pdf"
+        url={ResumePDF}
+      />
+
       <a href='https://drive.google.com/file/d/1KosFNaF5NmhxDCuZYeWfKVdIDWB3u-Wn/view?usp=sharing'
       target='_blank'>
         <button>Download</button>
